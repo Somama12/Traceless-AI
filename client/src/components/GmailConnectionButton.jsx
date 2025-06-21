a2b77b55632cb0d2dff7ab187034c5ba109d1f50
Somama Siddiqui <somamasiddiqui11@gmail.com>
2025-06-20 22:43:38 -0500

    The basic frontend is ready for now

---
+++ client/src/components/GmailConnectionButton.jsx
@@ -0,0 +1,12 @@
+const GmailConnectButton = () => {
+    return (
+      <a href="http://localhost:5000/auth">
+        <button className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-700 transition">
+          Connect Gmail
+        </button>
+      </a>
+    )
+  }
+  
+  export default GmailConnectButton;
+  
